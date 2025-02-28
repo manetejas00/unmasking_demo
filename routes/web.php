a1a9c5f0b8397fc021f72b70962250ad5e289dce
manetejas00 <75880380+manetejas00@users.noreply.github.com>
2025-02-28 14:24:46 +0530

    updated

--- routes/web.php
+++ routes/web.php
@@ -18,38 +18,38 @@ Route::get('/blog-details', [HomeController::class, 'blog_details'])->name('blog
 Route::get('/blog-details-2', [HomeController::class, 'blog_details_2'])->name('blog-details-2');
 Route::get('/team-1', [HomeController::class, 'team_1'])->name('team-1');
 Route::get('/team-details', [HomeController::class, 'team_details'])->name('team-details');
-Route::get('team-2','team_2')->name('team-2');
 Route::get('/contact', [HomeController::class, 'contact'])->name('contact');
 
 
 // Pages
 Route::prefix('pages/')->group(function () {
     Route::controller(PagesController::class)->group(function () {
-        Route::get('appoinment','appoinment')->name('appoinment');
-        Route::get('faq','faq')->name('faq');
-        Route::get('notFound','notFound')->name('notFound');
-        Route::get('pricing','pricing')->name('pricing');
-        Route::get('project-mesonary-v2','project_mesonary_v2')->name('project-mesonary-v2');
-        Route::get('project-mesonary','project_mesonary')->name('project-mesonary');
-        Route::get('project-slider','project_slider')->name('project-slider');
-        Route::get('project','project')->name('project');
-        Route::get('projects-details-2','projects_details_2')->name('projects-details-2');
-        Route::get('projects-details','projects_details')->name('projects-details');
-        Route::get('team','team')->name('team');
+        Route::get('appoinment', 'appoinment')->name('appoinment');
+        Route::get('faq', 'faq')->name('faq');
+        Route::get('notFound', 'notFound')->name('notFound');
+        Route::get('pricing', 'pricing')->name('pricing');
+        Route::get('project-mesonary-v2', 'project_mesonary_v2')->name('project-mesonary-v2');
+        Route::get('project-mesonary', 'project_mesonary')->name('project-mesonary');
+        Route::get('project-slider', 'project_slider')->name('project-slider');
+        Route::get('project', 'project')->name('project');
+        Route::get('projects-details-2', 'projects_details_2')->name('projects-details-2');
+        Route::get('projects-details', 'projects_details')->name('projects-details');
+        Route::get('team-2', 'team_2')->name('team-2');
+        Route::get('team', 'team')->name('team');
     });
 });
 
 // OnePage
 Route::prefix('onepage/')->group(function () {
     Route::controller(OnePageController::class)->group(function () {
-        Route::get('onepage-one','onePage_one')->name('onepage-one');
-        Route::get('onepage-two','onePage_two')->name('onepage-two');
-        Route::get('onepage-three','onePage_three')->name('onepage-three');
-        Route::get('onepage-four','onePage_four')->name('onepage-four');
-        Route::get('onepage-five','onePage_five')->name('onepage-five');
-        Route::get('onepage-six','onePage_six')->name('onepage-six');
-        Route::get('onepage-seven','onePage_seven')->name('onepage-seven');
-        Route::get('onepage-eight','onePage_eight')->name('onepage-eight');
+        Route::get('onepage-one', 'onePage_one')->name('onepage-one');
+        Route::get('onepage-two', 'onePage_two')->name('onepage-two');
+        Route::get('onepage-three', 'onePage_three')->name('onepage-three');
+        Route::get('onepage-four', 'onePage_four')->name('onepage-four');
+        Route::get('onepage-five', 'onePage_five')->name('onepage-five');
+        Route::get('onepage-six', 'onePage_six')->name('onepage-six');
+        Route::get('onepage-seven', 'onePage_seven')->name('onepage-seven');
+        Route::get('onepage-eight', 'onePage_eight')->name('onepage-eight');
     });
 });
 
@@ -57,12 +57,12 @@ Route::prefix('onepage/')->group(function () {
 // Multipages
 Route::prefix('multipages/')->group(function () {
     Route::controller(MultipagesController::class)->group(function () {
-        Route::get('index-two','index_two')->name('index-two');
-        Route::get('index-three','index_three')->name('index-three');
-        Route::get('index-four','index_four')->name('index-four');
-        Route::get('index-five','index_five')->name('index-five');
-        Route::get('index-six','index_six')->name('index-six');
-        Route::get('index-seven','index_seven')->name('index-seven');
-        Route::get('index-eight','index_eight')->name('index-eight');
+        Route::get('index-two', 'index_two')->name('index-two');
+        Route::get('index-three', 'index_three')->name('index-three');
+        Route::get('index-four', 'index_four')->name('index-four');
+        Route::get('index-five', 'index_five')->name('index-five');
+        Route::get('index-six', 'index_six')->name('index-six');
+        Route::get('index-seven', 'index_seven')->name('index-seven');
+        Route::get('index-eight', 'index_eight')->name('index-eight');
     });
 });
